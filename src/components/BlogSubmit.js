15ea2e66617772e8ff93ee6ba6449ce9380b41ed
Angelo Capozzi <acap613@gmail.com>
2020-03-17 20:18:34 -0400

    testing ajax

--- src/components/BlogSubmit.js
+++ src/components/BlogSubmit.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
+let axios = require('axios');
+
 class Button extends Component {
   handlePress(e) {
     if (this.props.onPress) {
         this.props.onPress(e);
     }
+    axios.put('http://18.221.127.241:3001/user/5e7022a44e0bde55d7d0b8d2/newjournal', {
+        daily_journal: "testing, testing, 123"
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
   render() {
     return (
         <TouchableOpacity
-            onPress={ this.handlePress.bind(this) }
+            onClick={ this.handlePress.bind(this) }
             style={ this.props.style } >
             <Text>{ this.props.children }</Text>
         </TouchableOpacity>
